a2d22b5c179e89a100aa3f5c0be225c1bdf9894e
Rafael <milewski@Rafaels-Mac-mini.local>
2017-03-29 12:52:09 +0800

    Removed multiverse stuff

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,14 +6,14 @@ if [ "$#" == 0 ] || [ "$#" -gt 1 ]; then
     DISTRO=xenial
 fi
 
-echo "deb http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe multiverse 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe multiverse 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse " \
+echo "deb http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe " \
 > /etc/apt/sources.list
