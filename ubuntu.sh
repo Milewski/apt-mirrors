4be8f26e7792294dae69ff928b3f8aba55c9b7b2
Rafael <milewski@Rafaels-Mac-mini.local>
2017-03-29 12:58:54 +0800

    Updated

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,14 +6,22 @@ if [ "$#" == 0 ] || [ "$#" -gt 1 ]; then
     DISTRO=xenial
 fi
 
-echo "deb http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe 
-deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe 
-deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe " \
+echo "deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}} main restricted
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-updates main restricted
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO} universe
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-updates universe
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-backports main restricted
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security main restricted
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security universe
+deb http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO} main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-updates main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO} universe
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-updates universe
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-backports main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security universe
+deb-src http://mirrors.aliyun.com/ubuntu/ ${DISTRO}-security multiverse" \
 > /etc/apt/sources.list
+
+
