e9668bdea9a110d04f3f5125e5feccd3fe594a0c
Rafael <milewski@Rafaels-Mac-mini.local>
2017-03-29 12:15:22 +0800

    removed unnecessary spaces

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,13 +7,13 @@ if [ "$#" == 0 ] || [ "$#" -gt 1 ]; then
 fi
 
 echo "deb http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe multiverse 
-      deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
-      deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
-      deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
-      deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse 
-      deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe multiverse 
-      deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
-      deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
-      deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
-      deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse " \
-      > /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
+deb http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO main restricted universe multiverse 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-security main restricted universe multiverse 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-updates main restricted universe multiverse 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-proposed main restricted universe multiverse 
+deb-src http://mirrors.aliyun.com/ubuntu/ $DISTRO-backports main restricted universe multiverse " \
+> /etc/apt/sources.list
